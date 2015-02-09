7374493c8047e1550573883a77159e7b1af1dafe
Zicore <zicore@gmx.de>
2015-02-09 12:43:23 +0100

    Order notification

---
+++ IntTrader.API/Base/Attributes/Request.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntTrader.API.Base.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class RequestCommand : Attribute
+    {
+        public RequestCommand(String command, bool isPublic)
+        {
+            Command = command;
+            IsPublic = isPublic;
+        }
+
+        public string Command { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
